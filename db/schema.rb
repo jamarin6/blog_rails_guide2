fd651592f613bfcd52a214275f01f2f93c16da3b
jamarin6 <jamarinhidalgo@gmail.com>
2014-04-21 18:29:01 +0000

    repairing user_id :(

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416121959) do
+ActiveRecord::Schema.define(version: 20140421180151) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20140416121959) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
