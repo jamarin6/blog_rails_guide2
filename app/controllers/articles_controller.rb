15ca6542c99daf04f98045d4f2b135091de98343
jamarin6 <jamarinhidalgo@gmail.com>
2014-04-22 11:53:15 +0000

    user_id ok:)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
+		@article.user = current_user 
  
        if @article.save
           redirect_to @article
