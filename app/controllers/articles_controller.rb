4844df0ea5a1cf381d7756c635a1574ac62996f9
jamarin6 <jamarinhidalgo@gmail.com>
2014-04-22 17:16:33 +0000

    problem with count num_articles at the moment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,7 @@ class ArticlesController < ApplicationController
 		@article.user = current_user 
  
        if @article.save
+       	#  @article.user.num_articles = @article.user.num_articles + 1
           redirect_to @article
       else
       	render 'new'
